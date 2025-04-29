b4ee12308fea82dab2fcb6fb29ccbdaa3dd0c0cf
Leandro <qazuor@gmail.com>
2025-04-29 18:33:02 +0000

    remove better-auth package

--- packages/generator/templates/packages/better-auth/TemplateContextVars.ts
+++
@@ -1,6 +0,0 @@
-import type { PackageConfig } from '../../../src/types/package.js';
-import type { ContextForTemplate } from '../../../src/utils/package-manager.js';
-
-export const getContextVars = (pkg: PackageConfig, context: ContextForTemplate) => {
-    return { providers: pkg.selectedConfig || [] };
-};
